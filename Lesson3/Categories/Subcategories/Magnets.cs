4504d1deaf328ae29c0baa0f001cce2f5f371cca
RitaMalinnikova <rita_malina@bk.ru>
2023-02-12 23:29:49 +0300

    struct

---
+++ Lesson3/Categories/Subcategories/Magnets.cs
@@ -0,0 +1,13 @@
+﻿using Lesson3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3.Categories.Subcategories
+{
+    internal class Magnets : Souvenirs
+    {
+    }
+}
