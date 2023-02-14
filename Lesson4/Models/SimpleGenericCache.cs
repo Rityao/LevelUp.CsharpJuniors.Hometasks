590bb8c642254b838fc51f0574f266177fa2bccd
RitaMalinnikova <rita_malina@bk.ru>
2023-02-14 21:24:14 +0300

    new lesson4

---
+++ Lesson4/Models/SimpleGenericCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4.Models
+{
+    internal class SimpleGenericCache<T>
+    {
+        private readonly Dictionary<string, T?> _cache = new();
+
+        internal void Store(string key, T value)
+        {
+            _cache[key] = value;
+        }
+
+        internal T? Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+
+            return default;
+        }
+    }
+}
+
