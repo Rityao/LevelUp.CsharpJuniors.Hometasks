17ce89e22f332b5ad9cf46ef6cd4242494c26fa6
RitaMalinnikova <rita_malina@bk.ru>
2023-02-26 17:24:47 +0300

    убран модификатор partial, добавлены комментарии в Program.cs

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Lesson4.Models;
 
-var sgc = new SimpleGenericCache<string>();
-sgc.Store("1","Cache?");
-Console.WriteLine(sgc.Fetch("1"));
+var sgc = new SimpleGenericCache<string>(); // создаём экземпляр класса и тип string будет использоваться этим классом
+sgc.Store("1","Cache?"); // вызвали метод Store, третий параметр не задали, поэтому подтянется автоматически таймаут = 30
+Console.WriteLine(sgc.Fetch("1")); // выводим информацию о записи кеша
 
 //значение CreationTime задаётся раньше чем сравнивается в методе Fetch (при timeout = 0 -- CreationTime < DateTime.now и значение удаляется)
 sgc.Store("2", "Cache2?", 0);
