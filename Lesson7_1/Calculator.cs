6be6a7b430ea4431d398a656045d77ab251ddb28
RitaMalinnikova <rita_malina@bk.ru>
2023-02-27 23:38:28 +0300

    v1

---
+++ Lesson7_1/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson7_1
+{
+    internal class Calculator<T>
+    {   
+        public Calculator(List<int> numbers) 
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                double sroot;
+                sroot = Math.Sqrt(numbers[i]);
+                Console.WriteLine("number " + numbers[i] + ", square root = " + sroot);
+            }
+        }
+    }
+}
