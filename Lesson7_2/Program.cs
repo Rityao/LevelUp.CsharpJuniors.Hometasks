f2e24fcfb308c6184d53eb561bd16e0da2fc48d1
rita_malina <rita_malina@bk.ru>
2023-03-04 14:21:06 +0300

    fix: lesson7_2

--- Lesson7_2/Program.cs
+++ Lesson7_2/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Text;
-using static System.Net.Mime.MediaTypeNames;
-
+﻿using System.Text;
 internal class Program
 {
     private static async Task Main(string[] args) 
@@ -14,16 +11,16 @@ internal class Program
         {
             DirectoryInfo di = new(path);
             foreach (var fi in di.GetFiles())
-            {
+            {   
+                string fiName = Path.Combine(path,fi.Name); 
                 using FileStream fstream = new(fullPath, FileMode.Append);  // запись в файл. если файл существует, то текст добавляется в конец файл. Если файла нет, то он создается
-                await string? text = File.ReadAllText(path + "\\" + fi.Name);
-                //using FileStream fstreamR = new(path + "\\" + fi.Name, FileMode.Open);
-                //byte[] buffer = new byte[fstreamR.Length]; // выделяем массив для считывания данных из файла
-                //await fstreamR.ReadAsync(buffer); // считываем данные
-                byte[] buffer = Encoding.Default.GetBytes(text + "\n");
-                await fstream.WriteAsync(buffer);                       // запись массива байтов в файл
-                 
-                Console.WriteLine($"Текст из файла {path + "\\" + fi.Name} записан");
+                using FileStream fstreamR = new(fiName, FileMode.Open); //экземпляр для чтения из файла
+                byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+                byte[] buffer = new byte[fstreamR.Length]; // выделяем массив для считывания данных из файла
+                await fstreamR.ReadAsync(buffer); // считываем данные
+                await fstream.WriteAsync(buffer); // запись массива байтов в файл
+                await fstream.WriteAsync(newline); // нужно разделить данные от файлов
+                Console.WriteLine($"Текст из файла {fiName} записан");
             }
             
             using (FileStream fstream = File.OpenRead(fullPath)) // чтение из файла
@@ -31,12 +28,12 @@ internal class Program
                 byte[] buffer = new byte[fstream.Length];                     // выделяем массив для считывания данных из файла
                 await fstream.ReadAsync(buffer);                              // считываем данные
                 string textFromFile = Encoding.Default.GetString(buffer);     // декодируем байты в строку
-                Console.WriteLine($"Текст из файла {fullPath}:\n{textFromFile}");
+                Console.WriteLine($"Текст из нового файла {fullPath}:\n{textFromFile}");
             }
         }
         catch (DirectoryNotFoundException)
         {
-            Console.WriteLine($"The directory was not found. Please, restart this program.");
+            Console.WriteLine($"The directory was not found. Please, restart the program.");
         }
     }
 }
