ea768cfce5f50ce17926525357c0432e73291710
rita_malina <rita_malina@bk.ru>
2023-03-04 15:32:44 +0300

    fix: lesson7_1

--- Lesson7_1/Calculator.cs
+++ Lesson7_1/Calculator.cs
@@ -1,15 +1,20 @@
-﻿namespace Lesson7_1
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+namespace Lesson7_1
 {
     internal class Calculator<T>
-    {   
-        public Calculator(List<int> numbers) 
+    {
+        public Calculator(List<int> numbers)
         {
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                double sroot;
-                sroot = Math.Sqrt(numbers[i]);
-                Console.WriteLine("number " + numbers[i] + ", square root = " + sroot);
-            }
+            int Square(int n) => n * n;
+            var array = numbers.Cast<int>().ToArray();
+            var squares = from n in array.AsParallel().AsOrdered()
+                          where n > 0
+                          select Square(n);
+
+            foreach (var f in squares)
+                Console.WriteLine("number " + Math.Sqrt(f) + ", square root = " + f);
         }
     }
 }
