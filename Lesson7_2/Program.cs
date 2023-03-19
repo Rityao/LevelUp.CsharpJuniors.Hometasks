f37ce0a65e005eb79020c0aa73e6a63dbff5b6ce
RitaMalinnikova <rita_malina@bk.ru>
2023-03-19 22:01:30 +0300

    FIX

--- Lesson7_2/Program.cs
+++ Lesson7_2/Program.cs
@@ -5,40 +5,49 @@ internal class Program
     {
         Console.WriteLine("Please, write the path of the directory");
         var path = Console.ReadLine() ?? string.Empty;
-        string fileName = @"\Lesson7_2__"+ DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") +".txt";
-        string fullPath = path + fileName;
-        try 
+        try
         {
+            var isDirectory = File.GetAttributes(path).HasFlag(FileAttributes.Directory); // проверка, что введён путь директории
+            string fileName = @"\Lesson7_2__" + DateTime.Now.ToString("dd_MM_yyyy__HH_mm_ss") + ".txt";
+            string fullPath = path + fileName;
             DirectoryInfo di = new(path);
             int i = 0;
             foreach (var fi in di.GetFiles())
             {   
                 string fiName = Path.Combine(path,fi.Name); 
-                using FileStream fstream = new(fullPath, FileMode.Append);  // запись в файл. если файл существует, то текст добавляется в конец файл. Если файла нет, то он создается
-                using FileStream fstreamR = new(fiName, FileMode.Open); //экземпляр для чтения из файла
-                byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
-                byte[] buffer = new byte[fstreamR.Length]; // выделяем массив для считывания данных из файла
-                await fstreamR.ReadAsync(buffer); // считываем данные
+                using FileStream fWriteStream = new(fullPath, FileMode.Append);  // запись в файл. если файл существует, то текст добавляется в конец файл. Если файла нет, то он создается
+                using FileStream fReadStream = new(fiName, FileMode.Open); //экземпляр для чтения из файла
+                byte[] newline = Encoding.UTF8.GetBytes(Environment.NewLine);
+                byte[] buffer = new byte[fReadStream.Length]; // выделяем массив для считывания данных из файла
+                await fReadStream.ReadAsync(buffer); // считываем данные
                 if (i != 0)
                 {
-                    await fstream.WriteAsync(newline); // нужно разделить данные от файлов
+                    await fWriteStream.WriteAsync(newline); // нужно разделить данные от файлов
                 }
-                await fstream.WriteAsync(buffer); // запись массива байтов в файл
+                await fWriteStream.WriteAsync(buffer); // запись массива байтов в файл
                 Console.WriteLine($"Текст из файла {fiName} записан");
                 i++;
             }
-            
-            using (FileStream fstream = File.OpenRead(fullPath)) // чтение из файла
+
+            if (i != 0) // проверка, что в папке были файлы, которые конкатенировались
             {
-                byte[] buffer = new byte[fstream.Length];                     // выделяем массив для считывания данных из файла
-                await fstream.ReadAsync(buffer);                              // считываем данные
-                string textFromFile = Encoding.Default.GetString(buffer);     // декодируем байты в строку
-                Console.WriteLine($"Текст из нового файла {fullPath}:\n{textFromFile}");
+                using (FileStream fstream = File.OpenRead(fullPath)) // чтение из файла
+                {
+                    byte[] buffer = new byte[fstream.Length];                     // выделяем массив для считывания данных из файла
+                    await fstream.ReadAsync(buffer);                              // считываем данные
+                    string textFromFile = Encoding.UTF8.GetString(buffer);     // декодируем байты в строку
+                    Console.WriteLine($"Текст из нового файла {fullPath}:\n{textFromFile}");
+                }
             }
+            else Console.WriteLine($"This directory is empty!)");
         }
         catch (DirectoryNotFoundException)
         {
             Console.WriteLine($"The directory was not found. Please, restart the program.");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"It`s not a directory!) Please, restart the program.");
         }
     }
 }
